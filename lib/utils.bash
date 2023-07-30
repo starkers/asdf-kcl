5c4378da8d879512b0736f8c3bc0fd9f5a2851c8
David Stark <david@starkers.org>
2023-07-30 04:32:14 +0100

    no need for set -x

--- lib/utils.bash
+++ lib/utils.bash
@@ -86,7 +86,6 @@ install_version() {
 
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		set -x
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"

2f55c30aafd07b71a21196db262afe6114ffae84
David Stark <david@starkers.org>
2023-07-30 04:30:23 +0100

    misc update

--- lib/utils.bash
+++ lib/utils.bash
@@ -82,9 +82,7 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "${ASDF_DOWNLOAD_PATH}"/bin/kcl "${install_path}"
-		cp -r "${ASDF_DOWNLOAD_PATH}"/bin/kcl-* "${install_path}"
-		cd "${install_path}"
+		cp -r "${ASDF_DOWNLOAD_PATH}"/bin/* "${install_path}"
 
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
